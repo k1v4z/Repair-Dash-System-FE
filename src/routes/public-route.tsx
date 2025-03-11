6c8ac1608a7904f132317dcc77edaa330da15094
k1v4z <addnguyen32@gmail.com>
2025-03-11 21:22:01 +0700

    chore: update router

--- src/routes/public-route.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import type { PrivateRouteProps } from "../types/routes.type";
-import { useAuth } from "@/features/auth/hooks/useAuth";
-
-export const PublicRoute = ({ children }: PrivateRouteProps) => {
-  const { isAuthenticated } = useAuth();
-  return !isAuthenticated ? children : <Navigate to="/" />;
-};
